cca499b3704f8a17afa35d267aaab9228cd8c54a
hooss <hoosshusu@gmail.com>
2020-06-29 22:22:15 +0900

    Updates

---
+++ precache-manifest.ddda5db58de3c93288e327bc8100c193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d930be8c42cd1ffec7ba48c4b7ba7cf",
+    "url": "/today-class/index.html"
+  },
+  {
+    "revision": "a8a2cc0431b337bad796",
+    "url": "/today-class/static/css/main.222189dd.chunk.css"
+  },
+  {
+    "revision": "15b26ef6a9d8d1a83966",
+    "url": "/today-class/static/js/2.47646e0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/today-class/static/js/2.47646e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a2cc0431b337bad796",
+    "url": "/today-class/static/js/main.f127861c.chunk.js"
+  },
+  {
+    "revision": "54e9d47ce1cd1dea82c5",
+    "url": "/today-class/static/js/runtime-main.0ba4bafd.js"
+  }
+]);
